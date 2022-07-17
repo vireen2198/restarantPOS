a7d4d8c6cb0f98c3c24d3d396cb270cc364c70f4
Rajveender Singh <72578341+Rajveenz2@users.noreply.github.com>
2022-07-18 01:50:39 +0800

    Update products.js
    Avoid duplicate productName

--- models/Products.js
+++ models/Products.js
@@ -3,13 +3,12 @@ var mongoose = require('mongoose');
 var db = require('../db');
 
 var ProductsSchema = new mongoose.Schema({
-    productId: { type: mongoose.Types.ObjectId },
     productName: String,
     productPrice: String,
     productDescription: String,
     productImageAddress: String,
 });
 
-ProductsSchema.index({ productId: 1 });
+ProductsSchema.index({ productName: 1}, { unique: true });
 
-module.exports = db.model('Products', ProductsSchema);
+module.exports = db.model('Product', ProductsSchema);
