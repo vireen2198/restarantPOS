8f305da5764b7bcc99a47ec7c79fa11e17f61855
vireen2198 <109417472+vireen2198@users.noreply.github.com>
2022-07-17 15:40:01 +0800

    first commit

---
+++ javascripts/index.js
@@ -0,0 +1,12 @@
+const getElByClass=(elName,parent)=>{
+    return parent.querySelector(elName)
+}
+const getElementsByClass=(elName,parent=document)=>{
+    return parent.querySelectorAll(elName)
+}
+// const statsUi=[...getElementsByClass(".stats-ui")];
+// statsUi.map(el=>{
+//     const getRandom255=()=>{return Math.floor(Math.random()*255)+1};
+//     const randomColor=`rgb(${getRandom255()},${getRandom255()},${getRandom255()})`;
+//     el.style.backgroundColor=randomColor
+// })
